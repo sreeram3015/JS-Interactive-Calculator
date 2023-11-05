5ee4f21beca329d28317293704aafe2d4f5c0476
Sreeram P <psreeram3015@proton.me>
2023-11-05 16:30:09 +0530

    JavaScript function for appending operators to the output
    Implemented a JavaScript function appendOperator(operator) to add the provided operator to the output displayed on the calculator.
    Utilized the getElementById method to access the output element from the HTML file.
    
    Changes height of display.

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ let output = document.getElementById('output');
 function appendNumber(number){
     output.innerText += number;
 }
+
+function appendOperator(operator){
+    output.innerText += operator;
+}
